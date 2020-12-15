ecc0c78cc0598583c1980921ce527d99e52a9f22
Stephen <20730278+ssheld@users.noreply.github.com>
2020-12-15 08:38:42 -0500

    Change build process from docker to jar

--- src/main/docker/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "********************************************************"
-echo "Starting One Stop Gif Shop"
-echo "********************************************************"
-java -Djava.security.egd=file:/dev/./urandom -jar /usr/local/onestopgifshop/@project.build.finalName@.jar
